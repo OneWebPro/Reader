9b2b39ae2411a1df859e6efad2a86306dc8f8b0c
Maciej <biuro@onewebpro.pl>
2013-12-21 20:45:36 +0100

    Add dragiing.

--- src/main/java/views/Main.java
+++ src/main/java/views/Main.java
@@ -1,20 +1,62 @@
 package views;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
+
+    private double initialX = 0;
+    private double initialY = 0;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../views/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        addDragListeners(root);
         setUserAgentStylesheet(STYLESHEET_MODENA);
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
         primaryStage.setTitle("OneWebPro Reader");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
+    }
+
+    private void addDragListeners(final Node n){
+        n.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent me) {
+                if(me.getButton()!=MouseButton.MIDDLE)
+                {
+                    initialX = me.getSceneX();
+                    initialY = me.getSceneY();
+                }
+                else
+                {
+                    n.getScene().getWindow().centerOnScreen();
+                    initialX = n.getScene().getWindow().getX();
+                    initialY = n.getScene().getWindow().getY();
+                }
+
+            }
+        });
+
+        n.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent me) {
+                if(me.getButton()!=MouseButton.MIDDLE)
+                {
+                    n.getScene().getWindow().setX( me.getScreenX() - initialX );
+                    n.getScene().getWindow().setY( me.getScreenY() - initialY);
+                }
+            }
+        });
     }
 
 
