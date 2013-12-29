565683f7fc6fcd6c12258907329c7454ac0faf44
Maciej <biuro@onewebpro.pl>
2013-12-29 10:49:07 +0100

    Change to lambads

--- src/main/java/pl/onewebpro/controllers/MainController.java
+++ src/main/java/pl/onewebpro/controllers/MainController.java
@@ -10,41 +10,11 @@ import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
 
-    private double initialX = 0;
-    private double initialY = 0;
 
-    @FXML
-    private ToolBar menu;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        addDragListeners(menu);
+
     }
 
-
-
-    private void addDragListeners(final Node n){
-        n.setOnMousePressed(me -> {
-            if(me.getButton()!= MouseButton.MIDDLE)
-            {
-                initialX = me.getSceneX();
-                initialY = me.getSceneY();
-            }
-            else
-            {
-                n.getScene().getWindow().centerOnScreen();
-                initialX = n.getScene().getWindow().getX();
-                initialY = n.getScene().getWindow().getY();
-            }
-
-        });
-
-        n.setOnMouseDragged(me -> {
-            if(me.getButton()!=MouseButton.MIDDLE)
-            {
-                n.getScene().getWindow().setX( me.getScreenX() - initialX );
-                n.getScene().getWindow().setY( me.getScreenY() - initialY);
-            }
-        });
-    }
 }
---
+++ src/main/java/pl/onewebpro/controllers/MenuController.java
@@ -0,0 +1,50 @@
+package pl.onewebpro.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.ToolBar;
+import javafx.scene.input.MouseButton;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class MenuController implements Initializable {
+
+    private double initialX = 0;
+    private double initialY = 0;
+
+    @FXML
+    private ToolBar menu;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        addDragListeners(menu);
+    }
+
+    private void addDragListeners(final Node n){
+        n.setOnMousePressed(me -> {
+            if(me.getButton()!= MouseButton.MIDDLE)
+            {
+                initialX = me.getSceneX();
+                initialY = me.getSceneY();
+            }
+            else
+            {
+                n.getScene().getWindow().centerOnScreen();
+                initialX = n.getScene().getWindow().getX();
+                initialY = n.getScene().getWindow().getY();
+            }
+
+        });
+
+        n.setOnMouseDragged(me -> {
+            if(me.getButton()!=MouseButton.MIDDLE)
+            {
+                n.getScene().getWindow().setX( me.getScreenX() - initialX );
+                n.getScene().getWindow().setY( me.getScreenY() - initialY);
+            }
+        });
+    }
+}
