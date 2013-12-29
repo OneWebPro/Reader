34bc55180560360acc0d4b5888684c0798a943ae
Maciej <biuro@onewebpro.pl>
2013-12-29 15:51:37 +0100

    Work on filechooser.

--- src/main/java/pl/onewebpro/controllers/MenuController.java
+++ src/main/java/pl/onewebpro/controllers/MenuController.java
@@ -2,8 +2,14 @@ package pl.onewebpro.controllers;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -21,11 +27,36 @@ public class MenuController implements Initializable {
     @FXML
     public MenuItem about;
 
+    @FXML
+    public Menu menu;
+
+    @FXML
+    public MenuBar menuBar;
+
+    final FileChooser fileChooser = new FileChooser();
+
+    private Window stage;
+
+    FileChooser.ExtensionFilter pdfExt = new FileChooser.ExtensionFilter("Acrobat PDF documents", "*.pdf");
+    FileChooser.ExtensionFilter xpsExt = new FileChooser.ExtensionFilter("XPS documents", "*.xps");
+    FileChooser.ExtensionFilter cbzExt = new FileChooser.ExtensionFilter("Comic book documents", "*.cbz");
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         //Close button
         exit.setOnAction(ae -> {
             System.exit(0);
         });
+
+        open.setOnAction(ae ->{
+            fileChooser.getExtensionFilters().addAll(pdfExt,xpsExt,cbzExt);
+            File file = fileChooser.showOpenDialog(stage);
+            if(stage == null)
+                stage = menuBar.getScene().getWindow();
+            if (file != null) {
+                //TODO:Open file
+            }
+        });
+
     }
 }
